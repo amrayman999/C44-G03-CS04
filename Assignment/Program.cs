3543ed737371c78fba70785743f76f0d56356a3b
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-04 16:15:55 +0300

    Question 3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -18,20 +18,45 @@
             #endregion
 
             #region question 2
-            Console.Write("Enter a number: ");
-            int number = int.Parse(Console.ReadLine());
-            if(number < 0)
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if(number < 0)
+            //{
+            //    Console.WriteLine("Negative");
+            //}
+            //else if (number == 0)
+            //{
+            //    Console.WriteLine("Zero");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Positive");
+            //}
+            #endregion
+
+            #region Question 3
+            int[] numbers = new int[3];
+            Console.Write("Enter first number: ");
+            numbers[0] = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            numbers[1] = int.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            numbers[2] = int.Parse(Console.ReadLine());
+            int maxNumber = -999;
+            int minNumber = 999;
+            foreach (int number in numbers)
             {
-                Console.WriteLine("Negative");
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
             }
-            else if (number == 0)
-            {
-                Console.WriteLine("Zero");
-            }
-            else
-            {
-                Console.WriteLine("Positive");
-            }
+            Console.WriteLine($"Maximum element is: {maxNumber}");
+            Console.WriteLine($"Minimum element is: {minNumber}");
             #endregion
         }
     }
