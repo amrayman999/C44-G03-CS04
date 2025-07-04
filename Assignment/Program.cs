b168964cb9d64c15b471e1ca19f4872ee9e2afa3
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-04 16:04:12 +0300

    Question 1

---
+++ Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            Console.Write("Enter a number: ");
+            float number = float.Parse(Console.ReadLine());
+            if(number % 3 == 0 && number % 4 == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            #endregion
+        }
+    }
+}
